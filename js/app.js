e595fd5804c85736b050244001d498919eab15bb
Ryan <rmcguinn94@gmail.com>
2018-05-07 22:18:22 -0700

    Commented out test function

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ const portfolio = document.querySelector('.Portfolio');
 const intro = document.querySelector('.Intro-container');
 const projects = document.querySelector('.Dev-container');
 
-function bacon() {
-    if (header.classList.contains('b-loaded')) {
-        header.style.height = '140px';
-        console.log('Working');
-    }
-    console.log('Running');
-    // setTimeout(bacon(), 5000);
-}
-bacon();
+// function bacon() {
+//     if (header.classList.contains('b-loaded')) {
+//         header.style.height = '140px';
+//         console.log('Working');
+//     }
+//     console.log('Running');
+//     // setTimeout(bacon(), 5000);
+// }
+
 
 function aboutScroll() {
     intro.scrollIntoView({ block: 'start', behavior: 'smooth'});
